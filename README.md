6b650414280a50a3fa65868dc5f6d6d0e51923bb
Shaps Benkau <shapsuk@me.com>
2022-06-24 16:08:21 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftUIBackports
+
+A description of this package.
---
+++ Sources/SwiftUIBackports/SwiftUIBackports.swift
@@ -0,0 +1,6 @@
+public struct SwiftUIBackports {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
