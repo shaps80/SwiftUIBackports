8160eeecc2486feecdcdbf137956f8dcc733bf38
Shaps Benkau <shapsuk@me.com>
2022-06-25 02:32:10 +0100

    Removing all but iOS support for this release, until I've had a chance to property test

--- Package.swift
+++ Package.swift
@@ -7,9 +7,9 @@ let package = Package(
     name: "SwiftUIBackports",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_15),
-        .tvOS(.v13),
-        .watchOS(.v6)
+//        .macOS(.v10_15),
+//        .tvOS(.v13),
+//        .watchOS(.v6)
     ],
     products: [
         .library(
