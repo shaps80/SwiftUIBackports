b5f390ec9c5278efc668655f35c7961b3881c76a
Shaps Benkau <shapsuk@me.com>
2022-06-28 09:24:20 +0100

    Fixes macOS support

--- Sources/SwiftUIBackports/iOS 13+/Navigation/NavigationTitle.swift
+++ Sources/SwiftUIBackports/iOS 13+/Navigation/NavigationTitle.swift
@@ -1,19 +1,27 @@
 import SwiftUI
 
-#if !os(macOS)
-@available(iOS, introduced: 13, deprecated: 14)
-@available(watchOS, introduced: 6, deprecated: 7)
-@available(tvOS, introduced: 13, deprecated: 14)
+@available(iOS, deprecated: 14)
+@available(watchOS, deprecated: 7)
+@available(tvOS, deprecated: 14)
+@available(macOS)
 public extension Backport where Content: View {
 
     @ViewBuilder
     func navigationTitle<S: StringProtocol>(_ title: S) -> some View {
+        #if os(macOS)
+        content.navigationTitle(title)
+        #else
         content.navigationBarTitle(title)
+        #endif
     }
 
     @ViewBuilder
     func navigationTitle(_ titleKey: LocalizedStringKey) -> some View {
+        #if os(macOS)
+        content.navigationTitle(titleKey)
+        #else
         content.navigationBarTitle(titleKey)
+        #endif
     }
 
 }
