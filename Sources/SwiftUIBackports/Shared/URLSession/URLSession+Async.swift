26223a8b7308072103f1ffd38b0017c5385a761a
Shaps Benkau <shapsuk@me.com>
2022-07-08 11:08:44 +0100

    Adds URLSession backports for most async/await implementations (dataTask, downloadTask, uploadTask)

---
+++ Sources/SwiftUIBackports/Shared/URLSession/URLSession+Async.swift
@@ -0,0 +1,158 @@
+import Foundation
+
+@available(iOS, deprecated: 15.0)
+@available(macOS, deprecated: 12.0)
+@available(tvOS, deprecated: 15.0)
+@available(watchOS, deprecated: 8.0)
+public extension Backport where Content: URLSession {
+
+    /// Start a data task with a URL using async/await.
+    /// - parameter url: The URL to send a request to.
+    /// - returns: A tuple containing the binary `Data` that was downloaded,
+    ///   as well as a `URLResponse` representing the server's response.
+    /// - throws: Any error encountered while performing the data task.
+    func data(from url: URL) async throws -> (Data, URLResponse) {
+        try await data(for: URLRequest(url: url))
+    }
+
+    /// Start a data task with a `URLRequest` using async/await.
+    /// - parameter request: The `URLRequest` that the data task should perform.
+    /// - returns: A tuple containing the binary `Data` that was downloaded,
+    ///   as well as a `URLResponse` representing the server's response.
+    /// - throws: Any error encountered while performing the data task.
+    func data(for request: URLRequest) async throws -> (Data, URLResponse) {
+        let sessionTask = URLSessionTaskActor()
+
+        return try await withTaskCancellationHandler {
+            Task { await sessionTask.cancel() }
+        } operation: {
+            try await withCheckedThrowingContinuation { continuation in
+                Task {
+                    await sessionTask.start(content.dataTask(with: request) { data, response, error in
+                        guard let data = data, let response = response else {
+                            let error = error ?? URLError(.badServerResponse)
+                            continuation.resume(throwing: error)
+                            return
+                        }
+
+                        continuation.resume(returning: (data, response))
+                    })
+                }
+            }
+        }
+    }
+
+    func upload(for request: URLRequest, fromFile fileURL: URL) async throws -> (Data, URLResponse) {
+        let sessionTask = URLSessionTaskActor()
+        return try await withTaskCancellationHandler {
+            Task { await sessionTask.cancel() }
+        } operation: {
+            try await withCheckedThrowingContinuation { continuation in
+                Task {
+                    await sessionTask.start(content.uploadTask(with: request, fromFile: fileURL) { data, response, error in
+                        guard let data = data, let response = response else {
+                            let error = error ?? URLError(.badServerResponse)
+                            return continuation.resume(throwing: error)
+                        }
+
+                        continuation.resume(returning: (data, response))
+                    })
+                }
+            }
+        }
+    }
+
+    func upload(for request: URLRequest, from bodyData: Data) async throws -> (Data, URLResponse) {
+        let sessionTask = URLSessionTaskActor()
+        return try await withTaskCancellationHandler {
+            Task { await sessionTask.cancel() }
+        } operation: {
+            try await withCheckedThrowingContinuation { continuation in
+                Task {
+                    await sessionTask.start(content.uploadTask(with: request, from: bodyData) { data, response, error in
+                        guard let data = data, let response = response else {
+                            let error = error ?? URLError(.badServerResponse)
+                            return continuation.resume(throwing: error)
+                        }
+
+                        continuation.resume(returning: (data, response))
+                    })
+                }
+            }
+        }
+    }
+
+    func download(for request: URLRequest) async throws -> (URL, URLResponse) {
+        let sessionTask = URLSessionTaskActor()
+        return try await withTaskCancellationHandler {
+            Task { await sessionTask.cancel() }
+        } operation: {
+            try await withCheckedThrowingContinuation { continuation in
+                Task {
+                    await sessionTask.start(content.downloadTask(with: request) { data, response, error in
+                        guard let data = data, let response = response else {
+                            let error = error ?? URLError(.badServerResponse)
+                            return continuation.resume(throwing: error)
+                        }
+
+                        continuation.resume(returning: (data, response))
+                    })
+                }
+            }
+        }
+    }
+
+    func download(from url: URL) async throws -> (URL, URLResponse) {
+        let sessionTask = URLSessionTaskActor()
+        return try await withTaskCancellationHandler {
+            Task { await sessionTask.cancel() }
+        } operation: {
+            try await withCheckedThrowingContinuation { continuation in
+                Task {
+                    await sessionTask.start(content.downloadTask(with: url) { data, response, error in
+                        guard let data = data, let response = response else {
+                            let error = error ?? URLError(.badServerResponse)
+                            return continuation.resume(throwing: error)
+                        }
+
+                        continuation.resume(returning: (data, response))
+                    })
+                }
+            }
+        }
+    }
+
+    func download(resumeFrom resumeData: Data) async throws -> (URL, URLResponse) {
+        let sessionTask = URLSessionTaskActor()
+        return try await withTaskCancellationHandler {
+            Task { await sessionTask.cancel() }
+        } operation: {
+            try await withCheckedThrowingContinuation { continuation in
+                Task {
+                    await sessionTask.start(content.downloadTask(withResumeData: resumeData) { data, response, error in
+                        guard let data = data, let response = response else {
+                            let error = error ?? URLError(.badServerResponse)
+                            return continuation.resume(throwing: error)
+                        }
+
+                        continuation.resume(returning: (data, response))
+                    })
+                }
+            }
+        }
+    }
+
+}
+
+private actor URLSessionTaskActor {
+    weak var task: URLSessionTask?
+
+    func start(_ task: URLSessionTask) {
+        self.task = task
+        task.resume()
+    }
+
+    func cancel() {
+        task?.cancel()
+    }
+}
