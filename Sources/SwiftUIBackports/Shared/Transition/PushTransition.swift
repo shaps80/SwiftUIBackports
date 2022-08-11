920a67dc7517dd67d11be4517bdc507f8dedcdd1
drucelweisse <drucelweisse@gmail.com>
2022-08-11 21:06:32 +0300

    added backport property and extension to backport

--- Sources/SwiftUIBackports/Shared/Transition/PushTransition.swift
+++ Sources/SwiftUIBackports/Shared/Transition/PushTransition.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 extension AnyTransition {
-    
+    static var backport: Backport<AnyTransition>{
+        Backport(.identity)
+    }
+}
+
+extension Backport where Wrapped == AnyTransition {
     /// Creates a transition that when added to a view will animate the view’s insertion by moving it in from the specified edge while fading it in, and animate its removal by moving it out towards the opposite edge and fading it out.
     /// - Parameter edge: the edge from which the view will be animated in.
     /// - Returns: A transition that animates a view by moving and fading it.
@@ -17,7 +22,7 @@ extension AnyTransition {
     @available(macOS, deprecated: 13.0)
     @available(tvOS, deprecated: 16.0)
     @available(macCatalyst, deprecated: 16.0)
-    static func push(from edge: Edge) -> AnyTransition {
+    func push(from edge: Edge) -> AnyTransition {
         
         var oppositeEdge: Edge
         switch edge {
